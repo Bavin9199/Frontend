c6f7dc65024bfdc4665050773ba7bd9f4d9e6499
Bavin <11481814+bavin@user.noreply.gitee.com>
2024-07-24 11:44:29 +0800

    first commit

---
+++ javascript/demo/sidebar/sidebar.js
@@ -0,0 +1,10 @@
+function openSide(){
+    document.getElementById("side").style.width="250px";
+    document.getElementById("content").style.left="260px";
+}
+
+function closeSide(){
+    document.getElementById("side").style.width="0px";
+    document.getElementById("content").style.left="0px";
+    
+}
